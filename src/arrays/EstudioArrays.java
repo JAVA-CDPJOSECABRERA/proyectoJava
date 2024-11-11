0945131aab218dfd982c10d277176478cf4fcab0
Jesús Alcocer Fernández <jesusalcocerfdez@cdpjosecabrera.es>
2024-11-11 10:44:35 +0100

    crearmos el metodo ordenar

--- src/arrays/EstudioArrays.java
+++ src/arrays/EstudioArrays.java
@@ -1,5 +1,16 @@
 package arrays;
 
 public class EstudioArrays {
+    public EstudioArrays() {
+        System.out.println("HEMOS CREADO LA CLASE ESTUDIOARRAY");
+        ordenaArray();
+            }
+        
+            private void ordenaArray() {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'ordenaArray'");
+            }
+
 
 }
+ 
