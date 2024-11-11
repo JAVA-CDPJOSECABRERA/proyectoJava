898104c0d6d036364093e159c1e3325c96d07712
Jesús Alcocer Fernández <jesusalcocerfdez@cdpjosecabrera.es>
2024-11-11 10:54:36 +0100

    actualizamos proyecto

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("HEMOS CREADO LA CLASE ESTUDIOARRAY");
+        System.out.println("Vamos a revertir un commit");
         
     }
 
